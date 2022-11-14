fb0459b75a9fb55e9d01e21ee7134d5272a04175
2DavidGarcia4 <gacub20ru@gmail.com>
2022-11-13 21:05:54 -0600

    sda

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectMongo = exports.tokenBot = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.tokenBot = process.env.TOKEN_BOT, exports.connectMongo = process.env.CONNECT_MONGO;
