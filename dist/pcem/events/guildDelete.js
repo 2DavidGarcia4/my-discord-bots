f94c04bc2b47cdda5a3083a47b9f9c886d10b3e5
2DavidGarcia4 <gacub20ru@gmail.com>
2023-03-03 17:39:22 -0600

    remove role

--- dist/pcem/events/guildDelete.js
+++ dist/pcem/events/guildDelete.js
@@ -11,9 +11,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.guildDeleteEvent = void 0;
 const discord_js_1 = require("discord.js");
+const db_1 = require("../db");
 const utils_1 = require("../utils");
 const guildDeleteEvent = (guild, client) => __awaiter(void 0, void 0, void 0, function* () {
     const botData = yield (0, utils_1.getBotData)(client);
+    const usersData = yield (0, utils_1.getUsersData)(client);
+    if (usersData) {
+        const user = usersData === null || usersData === void 0 ? void 0 : usersData.find(f => f.guilds.some(s => s == guild.id)), rol = '851577906828148766';
+        const server = client.guilds.cache.get(db_1.botDB.serverId);
+        if (user) {
+            user.guilds.splice(user.guilds.indexOf(guild.id), 1);
+            const member = server === null || server === void 0 ? void 0 : server.members.cache.get(user.userId);
+            if (user.guilds.length == 0 && member) {
+                if (member.roles.cache.has(rol))
+                    member.roles.remove(rol);
+            }
+        }
+        yield (0, utils_1.updateUsersData)(client, usersData);
+    }
     const owner = guild.members.cache.get(guild.ownerId), channelLog = client.channels.cache.get((botData === null || botData === void 0 ? void 0 : botData.logs.guildDelete) || '');
     const guildRoles = guild.roles.cache.filter(f => !f.managed && f.id != guild.id).map(m => ({ posicion: m.position, nombre: m.name })).sort((a, b) => b.posicion - a.posicion).map(r => r.nombre).slice(0, 10).join(", ");
     const GuildDeleteEb = new discord_js_1.EmbedBuilder()
@@ -22,7 +37,7 @@ const guildDeleteEvent = (guild, client) => __awaiter(void 0, void 0, void 0, fu
         .setImage(guild.bannerURL({ size: 2048 }))
         .setTitle("➖ Kicked from a server")
         .setDescription(`${guild.name}\n${guild.description || '*No description*'}`)
-        .setFields({ name: `<:wer:920166217086537739> **Guild:**`, value: `🆔 ID: ${guild.id}\n📅 Created at <t:${Math.floor(guild.createdAt.valueOf() / 1000)}:F> *(<t:${Math.floor(guild.createdAt.valueOf() / 1000)}:R>)*`, inline: true }, { name: `👥 **Members:** ${guild.members.cache.size.toLocaleString()}`, value: `👤 Users: ${guild.members.cache.filter(fm => !fm.user.bot).size}\n🤖 Bots: ${guild.members.cache.filter(fb => fb.user.bot).size.toLocaleString()}`, inline: true }, { name: `🌈 **Roles:** ${guild.roles.cache.size}`, value: `${guildRoles}`, inline: true }, { name: `👑 **Owner:**`, value: `${owner === null || owner === void 0 ? void 0 : owner.user.tag}\n🆔 ${owner === null || owner === void 0 ? void 0 : owner.id}` })
+        .setFields({ name: `<:wer:920166217086537739> **Guild:**`, value: `🆔 ID: ${guild.id}\n📅 Created at <t:${Math.floor(guild.createdAt.valueOf() / 1000)}:F> *(<t:${Math.floor(guild.createdAt.valueOf() / 1000)}:R>)*`, inline: true }, { name: `👥 **Members:** ${guild.members.cache.size.toLocaleString()}`, value: `👤 Users: ${guild.members.cache.filter(fm => !fm.user.bot).size}\n🤖 Bots: ${guild.members.cache.filter(fb => fb.user.bot).size.toLocaleString()}`, inline: true }, { name: `🌈 **Roles:** ${guild.roles.cache.size}`, value: `${guildRoles || 'unkowow'}`, inline: true }, { name: `👑 **Owner:**`, value: `${owner === null || owner === void 0 ? void 0 : owner.user.tag}\n🆔 ${owner === null || owner === void 0 ? void 0 : owner.id}` })
         .setColor('Red')
         .setTimestamp();
     if (channelLog === null || channelLog === void 0 ? void 0 : channelLog.isTextBased())
--- src/pcem/events/guildDelete.ts
+++ src/pcem/events/guildDelete.ts
@@ -1,9 +1,26 @@
-import { Client, EmbedBuilder, Guild } from "discord.js";
-import { getBotData } from "../utils";
+import { AuditLogEvent, Client, EmbedBuilder, Guild } from "discord.js";
+import { botDB } from "../db";
+import { getBotData, getUsersData, updateUsersData } from "../utils";
 
 
 export const guildDeleteEvent = async (guild: Guild, client: Client) => {
   const botData = await getBotData(client)
+
+  const usersData = await getUsersData(client)
+
+  if(usersData){
+    const user = usersData?.find(f=> f.guilds.some(s=> s==guild.id)), rol = '851577906828148766'
+    const server = client.guilds.cache.get(botDB.serverId)
+    if(user){
+      user.guilds.splice(user.guilds.indexOf(guild.id), 1)
+      const member = server?.members.cache.get(user.userId)
+      if(user.guilds.length == 0 && member){
+        if(member.roles.cache.has(rol)) member.roles.remove(rol)
+      }
+    }
+    await updateUsersData(client, usersData)
+  }
+
   const owner = guild.members.cache.get(guild.ownerId), channelLog = client.channels.cache.get(botData?.logs.guildDelete || '')
   const guildRoles = guild.roles.cache.filter(f => !f.managed && f.id != guild.id).map(m => ({ posicion: m.position, nombre: m.name })).sort((a, b) => b.posicion - a.posicion).map(r => r.nombre).slice(0, 10).join(", ")
 
@@ -16,7 +33,7 @@ export const guildDeleteEvent = async (guild: Guild, client: Client) => {
   .setFields(
     { name: `<:wer:920166217086537739> **Guild:**`, value: `🆔 ID: ${guild.id}\n📅 Created at <t:${Math.floor(guild.createdAt.valueOf() / 1000)}:F> *(<t:${Math.floor(guild.createdAt.valueOf() / 1000)}:R>)*`, inline: true },
     { name: `👥 **Members:** ${guild.members.cache.size.toLocaleString()}`, value: `👤 Users: ${guild.members.cache.filter(fm => !fm.user.bot).size}\n🤖 Bots: ${guild.members.cache.filter(fb => fb.user.bot).size.toLocaleString()}`, inline: true },
-    { name: `🌈 **Roles:** ${guild.roles.cache.size}`, value: `${guildRoles}`, inline: true },
+    { name: `🌈 **Roles:** ${guild.roles.cache.size}`, value: `${guildRoles || 'unkowow'}`, inline: true },
     { name: `👑 **Owner:**`, value: `${owner?.user.tag}\n🆔 ${owner?.id}` },
   )
   .setColor('Red')
