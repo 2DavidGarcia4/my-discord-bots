a04cd4346a458b7b42a19c0fefefa835a306fb95
Dagadev <gacub20ru@gmail.com>
2023-08-13 16:21:12 -0600

    add SnackData type

--- src/Pepe-frog/types.d.ts
+++ src/Pepe-frog/types.d.ts
@@ -1,3 +1,26 @@
+export interface SnackData {
+  roles: {
+    verified: string
+    spamer: string
+    content: string
+    announcement: string
+    verifiedSpeech: string
+  }
+  channels: {
+    logs: string
+    ready: string
+    stats: string
+    suggestions: string
+    announcements: string
+  }
+  categories: {
+    verifieds: string
+  }
+  emojis: {
+    
+  }
+}
+
 export interface ModDb {
   id: string
   warns: number
