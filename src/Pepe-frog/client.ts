d85078266c45ccc6761eecb6e60a6401ae8a35a2
Dagadev <gacub20ru@gmail.com>
2023-08-13 16:09:55 -0600

    update routes

--- src/Pepe-frog/client.ts
+++ src/Pepe-frog/client.ts
@@ -1,65 +1,61 @@
-import { Client, Collection, EmbedBuilder } from "discord.js";
-import { FrogDb } from "./db";
+import { Client, type ClientEvents, Collection } from 'discord.js'
+import { FrogDb } from './db'
 import { readdirSync } from 'fs'
-import { ContextCommand, SlashCommand, TextCommand } from ".";
+import { ContextCommand, SlashCommand, TextCommand } from '.'
+
+export type EventName = keyof ClientEvents
 
 export class PepeFrogClient extends Client {
   public data = FrogDb  
   public slashCommands: Collection<string, SlashCommand> = new Collection()
   public contextCommands: Collection<string, ContextCommand> = new Collection()
   public textCommands: Collection<string, TextCommand> = new Collection()
+  public getGuildById(guildId: string) {
+    return this.guilds.cache.get(guildId)
+  }
+  public getChannelById(channelId: string) {
+    return this.channels.cache.get(channelId)
+  }
 
   constructor(){
-    super({
-      intents: 131071,
-      allowedMentions: {repliedUser: true},
-      failIfNotExists: false,
-    })
+    super({intents: 131071,})
   }
   
   public async start(token: string | undefined){  
     try {
-      // await connect(moongoseURL || '')
-      // console.log("🟢 Connected to the database.")
-
       const isDist = __dirname.includes('src') ? 'src/Pepe-frog' : 'dist/Pepe-frog'
-      console.log(isDist)
   
       this.loadEvents(isDist)
-      // this.loadTextCommands(isDist)
-      // this.loadSlashCommands(isDist)
-      // this.loadContextCommands(isDist)
+      this.loadTextCommands(isDist)
+      this.loadSlashCommands(isDist)
+      this.loadContextCommands(isDist)
       this.login(token)
+      // this.on()
       
     } catch (error) {
-      console.log("🔴 An error occurred while connecting to the database", error)
+      // console.log("🔴 An error occurred while connecting to the database", error)
+      console.log("🔴 An error occurred while starting the bot", error)
     }
   }
   
   private loadTextCommands(isDist: string): void {
-    readdirSync(`./${isDist}/commands/text/`).forEach(folder=> {
-      readdirSync(`./${isDist}/commands/text/${folder}`).forEach(async file=> {
-        const textCommand: TextCommand = new (require(`./commands/text/${folder}/${file}`).default)();
-        this.textCommands.set(textCommand.name, textCommand);
-      })
+    readdirSync(`./${isDist}/commands/text/`).forEach(file=> {
+      const textCommand: TextCommand = new (require(`./commands/text/${file}`).default)()
+      this.textCommands.set(textCommand.name, textCommand)
     })
   }
 
   private loadSlashCommands(isDist: string){
-    readdirSync(`./${isDist}/commands/slash/`).forEach(folder => {
-      readdirSync(`./${isDist}/commands/slash/${folder}`).filter(d=> !['add', 'remove', 'set'].some(s=> s==d)).forEach(async file=> {
-        const slashCommand: SlashCommand = new (require(`./commands/slash/${folder}/${file}`).default)();
-        this.slashCommands.set(slashCommand.struct.name, slashCommand)
-      })
+    readdirSync(`./${isDist}/commands/slash/`).forEach(file => {
+      const slashCommand: SlashCommand = new (require(`./commands/slash/${file}`).default)()
+      this.slashCommands.set(slashCommand.struct.name, slashCommand)
     })
   }
 
   private loadContextCommands(isDist: string){
-    readdirSync(`./${isDist}/commands/context/`).forEach(folder => {
-      readdirSync(`./${isDist}/commands/context/${folder}`).forEach(async file=> {
-        const contextCommand: ContextCommand = new (require(`./commands/context/${folder}/${file}`).default)();
-        this.contextCommands.set(contextCommand.struct.name, contextCommand)
-      })
+    readdirSync(`./${isDist}/commands/context/`).forEach(file => {
+      const contextCommand: ContextCommand = new (require(`./commands/context/${file}`).default)()
+      this.contextCommands.set(contextCommand.struct.name, contextCommand)
     })
   }
 
