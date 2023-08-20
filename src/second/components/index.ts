9e3e06192dc13962e95fe83fb68308cdc9290933
Dagadev <gacub20ru@gmail.com>
2023-08-19 19:35:42 -0600

    export manage automatic content

--- src/second/components/index.ts
+++ src/second/components/index.ts
@@ -1,3 +1,4 @@
 export { Moderation } from './moderation'
 export { Announcements } from './announcements'
 export { Reactions } from './reactions'
+export { ManageAutomaticContent } from './automaticContent'
