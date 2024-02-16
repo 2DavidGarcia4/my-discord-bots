3fab762f3c6fcb2f77f3cd0029a3b442ac71f0ab
Dagadev <gacub20ru@gmail.com>
2024-02-15 18:27:26 -0600

    Add TYPES_CONTENT_IGNORE env variable

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,5 @@ config()
 export const connectMongo = process.env.CONNECT_MONGO, firstToken = process.env.FIRST_BOT_TOKEN, 
 secondToken = process.env.SECOND_BOT_TOKEN, 
 notionToken = process.env.NOTION_TOKEN,
-inDevelopment = process.env.DEVELOPMENT
+inDevelopment = process.env.DEVELOPMENT,
+TYPES_CONTENT_IGNORE = process.env.TYPES_CONTENT_IGNORE
