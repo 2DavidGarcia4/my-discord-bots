d685b397a790ff17700668d8cf2120df10fc8023
Dagadev <gacub20ru@gmail.com>
2024-02-15 18:28:52 -0600

    Compiled to version 2.16.16

--- dist/second/components/automaticContent.js
+++ dist/second/components/automaticContent.js
@@ -3,87 +3,78 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ManageAutomaticContent = void 0;
 const discord_js_1 = require("discord.js");
 const config_1 = require("../../config");
-const node_path_1 = require("node:path");
-const channels = {
-    martine: '1139600091829776498',
-    onlyNudes: '1139600102445551646'
-};
-const categories = {
-    martine: '949861762902138941',
-    martine2: '1141400243645190304',
-    onlyNudes: '1141075333374804059'
-};
+const config_2 = require("../../config");
+const martineChannel = '1139600091829776498';
+const martineCategories = [
+    '949861762902138941',
+    '1141400243645190304',
+    '1207834923734663188',
+    '1207834958287208458',
+    '1207841886123991080'
+];
 const autoContentServerId = '949861760096145438';
 async function ManageAutomaticContent(msg, client) {
-    if (config_1.inDevelopment)
+    if (config_1.inDevelopment || !config_2.TYPES_CONTENT_IGNORE)
         return;
     const { channelId, content } = msg;
-    if (![channels.martine, channels.onlyNudes].some(s => s == channelId))
+    if (channelId !== martineChannel)
         return;
-    const getAndSendContent = async (contentUrl, channel) => {
-        // return channel.send({content: `[**File url**](${contentUrl})`})
-        if (contentUrl.slice(contentUrl.length - 7, contentUrl.length).includes('.')) {
-            const response = await fetch(contentUrl);
-            if (response.status !== 200)
-                return;
-            const contentLength = response.headers.get('content-length');
-            const mb = 1048576;
-            let MBs = 0;
-            const fileExtension = (0, node_path_1.extname)(contentUrl).slice(1);
-            if (contentLength === null) {
-                const imageBufer = await response.arrayBuffer();
-                const buffer = Buffer.from(imageBufer);
-                MBs = buffer.length / mb;
-            }
-            else {
-                MBs = parseInt(contentLength) / mb;
-            }
-            //* 25MB max
-            if (MBs > 24)
-                return channel.send({ content: `[**File url**](${contentUrl}) ${fileExtension} | **${MBs.toFixed(2)} MB**` });
-            const fileNumber = (parseInt(channel.topic?.match(/\d+/g)?.[0] || '0')) + 1;
-            if (!config_1.inDevelopment)
-                channel.send({
-                    content: `**file${fileNumber}.${fileExtension}** | **${MBs.toFixed(2)} MB**`,
-                    files: [{ attachment: contentUrl, name: `file${fileNumber}.${fileExtension}` }]
-                }).then(() => {
-                    channel.edit({ topic: fileNumber + '' });
-                })
-                    .catch(e => console.error('Error in send file:', e));
+    const splitContent = content.replaceAll('`', '').split('\n');
+    const categoryName = splitContent.find(f => f.toLowerCase().includes('category:'))?.split(/ +/g).at(-1)?.toLowerCase();
+    const fileUrl = splitContent.find(f => f.toLowerCase().includes('image:'))?.split(/ +/g).pop();
+    // console.log({categoryName, fileUrl})
+    if (!(categoryName && fileUrl) || config_2.TYPES_CONTENT_IGNORE.split(/ +/g).some(s => s === categoryName))
+        return;
+    const autoContentServer = client.getGuildById(autoContentServerId);
+    if (fileUrl.slice(fileUrl.length - 7, fileUrl.length).includes('.')) {
+        const response = await fetch(fileUrl);
+        if (response.status !== 200)
+            return;
+        const contentType = response.headers.get('content-type');
+        if (contentType === null) {
+            console.log(`El archivo no tiene content-type header | URL: ${fileUrl}`);
+            return;
+        }
+        const channelName = `${categoryName}-${contentType.split('/')[0]}`;
+        const channel = autoContentServer?.channels.cache.find(f => f.name === channelName) ?? await autoContentServer?.channels.create({
+            name: channelName,
+            parent: martineCategories.find(c => autoContentServer.channels.cache.filter(f => c === f.parentId).size < 50),
+            nsfw: true
+        });
+        if (channel?.type !== discord_js_1.ChannelType.GuildText)
+            return;
+        const contentLength = response.headers.get('content-length');
+        const mbSize = 1048576;
+        const fileExtension = fileUrl.split('/').at(-1);
+        let MBs = 0;
+        if (contentLength === null) {
+            const imageBufer = await response.arrayBuffer();
+            const buffer = Buffer.from(imageBufer);
+            MBs = buffer.length / mbSize;
         }
         else {
-            if (!config_1.inDevelopment)
-                channel.send({ content: `[**File url**](${contentUrl})` });
+            MBs = parseInt(contentLength) / mbSize;
         }
-    };
-    const handleSendContent = (categoryId, categoryName, contentUrl, lastCategoriId) => {
-        const autoContentServer = client.getGuildById(autoContentServerId);
-        const martineChannel = autoContentServer?.channels.cache.find(f => (f.parentId == categoryId || f.parentId == lastCategoriId) && f.name == categoryName);
-        const firtCategoryChannels = autoContentServer?.channels.cache.filter(f => f.parentId == categoryId).size;
-        if (martineChannel?.type == discord_js_1.ChannelType.GuildText) {
-            getAndSendContent(contentUrl, martineChannel);
-        }
-        else {
-            autoContentServer?.channels.create({ name: categoryName, parent: firtCategoryChannels == 50 ? lastCategoriId : categoryId, nsfw: true }).then(newChannel => {
-                getAndSendContent(contentUrl, newChannel);
-            });
-        }
-    };
-    if (channelId == channels.martine) {
-        const splitContent = content.replaceAll('`', '').split('\n');
-        const categoryName = splitContent.find(f => f.toLowerCase().includes('category:'))?.split(/ +/g).pop();
-        const contentUrl = splitContent.find(f => f.toLowerCase().includes('image:'))?.split(/ +/g).pop();
-        // console.log({categoryName, contentUrl})
-        if (categoryName && contentUrl)
-            handleSendContent(categories.martine, categoryName.toLowerCase(), contentUrl, categories.martine2);
+        //* 25MB max
+        if (MBs > 24)
+            return channel.send({ content: `[**File url**](${fileUrl}) **${contentType}** | **${MBs.toFixed(2)} MB**` });
+        const fileNumber = (parseInt(channel.topic?.match(/\d+/g)?.[0] || '0')) + 1;
+        channel.send({
+            content: `**file${fileNumber}.${fileExtension}** | **${MBs.toFixed(2)} MB**`,
+            files: [{ attachment: fileUrl, name: `file${fileNumber}.${fileExtension}` }]
+        }).then(() => {
+            channel.edit({ topic: fileNumber + '' });
+        }).catch(e => console.error('Error in send file: ', e));
     }
-    if (channelId == channels.onlyNudes) {
-        const splitContent = content.split(/ +/g);
-        const lastName = splitContent[3];
-        const categoryName = splitContent[2] + (lastName.toLowerCase().includes('content') ? '' : '-' + lastName);
-        const contentUrl = splitContent.find(f => f.includes('http'))?.replace(')', '').split('(').pop();
-        if (categoryName && contentUrl)
-            handleSendContent(categories.onlyNudes, categoryName.toLowerCase(), contentUrl);
+    else {
+        const defaultChannel = autoContentServer?.channels.cache.find(f => f.name === categoryName) ?? await autoContentServer?.channels.create({
+            name: categoryName,
+            parent: martineCategories.find(c => autoContentServer.channels.cache.filter(f => c === f.parentId).size < 50),
+            nsfw: true
+        });
+        if (defaultChannel?.type === discord_js_1.ChannelType.GuildText) {
+            defaultChannel.send({ content: `[**File url**](${fileUrl})` });
+        }
     }
 }
 exports.ManageAutomaticContent = ManageAutomaticContent;
